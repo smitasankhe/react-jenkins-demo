1042c7d6e7912304ebcdfda6a9f7c35f30f13ad5
smitasankhe <smitasankhe@somaiya.edu>
2026-07-29 13:07:03 +0530

    Initial React Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Jenkins CI/CD Demo</h1>
+       <h1>React Jenkins webhook CI/CD Demo</h1>
+
+      <h2>Hello Jenkins!</h2>
+
+      <p>
+        This project is deployed using GitHub and Jenkins Pipeline.
+      </p>
+
+      <button>Build Success</button>
     </div>
   );
 }
